70c6d5bb66d56fd84e76a9d0406f14f93c452c4c
Gustavo Flores Romero <guxtavo825@gmail.com>
2022-06-14 16:46:29 -0500

    Se agregaron pruebas para la Pipeline
    Co-Authored-By: MarianaYaz <62482852+MarianaYaz@users.noreply.github.com>

--- test/test_api.py
+++ test/test_api.py
@@ -1,2 +1,52 @@
-def test_AddPlace():
-    assert True
+import http.client as httpClient
+import json
+
+
+
+def test_Login():
+    global token
+    global refresh_token
+    connection = httpClient.HTTPConnection("127.0.0.1",9090)
+    data = {
+        "enrollment": "zs19014017", 
+        "password": "12345", 
+    }
+    data2 = json.dumps(data)
+    headers = {"Content-Type": "application/json"}
+    response = connection.request(
+        "POST",
+        "/token",
+        data2,
+        headers,
+    )
+    generalResponse = connection.getresponse()
+    readResponse = generalResponse.read()
+    json_format = json.loads(readResponse)
+    statusCode = generalResponse.status
+    token = json_format["access_token"]
+    refresh_token = json_format["refresh_token"]
+    assert 200 == statusCode
+
+def test_Refresh():
+    global token
+    global refresh_token
+    connection = httpClient.HTTPConnection("127.0.0.1",9090)
+    data = {
+        "enrollment": "zs19014017", 
+        "password": "12345", 
+    }
+    bear = "Bearer " + refresh_token
+    data2 = json.dumps(data)
+    headers = {"Content-Type": "application/json", "Authorization" : bear}
+    response = connection.request(
+        "POST",
+        "/refresh",
+        data2,
+        headers,
+    )
+    generalResponse = connection.getresponse()
+    readResponse = generalResponse.read()
+    json_format = json.loads(readResponse)
+    statusCode = generalResponse.status
+    token = json_format["token"]
+    assert 200 == statusCode
