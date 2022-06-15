564cf28d86ec254981f067787a3b5d6e4575f471
Gustavo Flores Romero <guxtavo825@gmail.com>
2022-06-15 00:30:02 -0500

    Added more tests
    Co-Authored-By: MarianaYaz <62482852+MarianaYaz@users.noreply.github.com>

--- test/test_api.py
+++ test/test_api.py
@@ -1,4 +1,5 @@
 
+from cgi import test
 from http import HTTPStatus
 import http.client as httpClient
 import json
@@ -25,13 +26,34 @@ def test_PostStudent():
     statusCode = generalResponse.status
     assert HTTPStatus.CREATED == statusCode
 
+def test_PostAdmin():
+    connection = httpClient.HTTPConnection("127.0.0.1",9090)
+    data = {
+        "name" : "Admin",
+        "enrollment": "admin",
+        "password": "12345",
+        "status": 1,
+        "rol" : "administrador",
+        "degree" : "Lic. en Ingeniería de Software"
+    }
+    data2 = json.dumps(data)
+    headers = {"Content-Type": "application/json"}
+    response = connection.request(
+        "POST",
+        "/estudiantes",
+        data2,
+        headers,
+    )
+    generalResponse = connection.getresponse()
+    statusCode = generalResponse.status
+    assert HTTPStatus.CREATED == statusCode
 
 def test_Login():
     global token
     global refresh_token
     connection = httpClient.HTTPConnection("127.0.0.1",9090)
     data = {
-        "enrollment": "zs19014020",
+        "enrollment": "admin",
         "password": "12345",
     }
     data2 = json.dumps(data)
@@ -86,3 +108,79 @@ def test_GetStudent():
     generalResponse = connection.getresponse()
     statusCode = generalResponse.status
     assert statusCode == HTTPStatus.OK
+
+def test_PostPlace():
+    global token
+    connection = httpClient.HTTPConnection("127.0.0.1",9090)
+    data = {
+        "name": "Bola de Oro",
+        "address": "Calle Centro #25",
+        "services" : "Ofrecen cafe muy rico",
+        "schedule" : "Lunes 7:00-8:00, Martes 7:00-8:00",
+        "type_place" : "Comida",
+        "image" : "010101",
+        "status" : 1
+    }
+    bear = "Bearer " + token
+    data2 = json.dumps(data)
+    headers = {"Content-Type": "application/json", "Authorization" : bear}
+    response = connection.request(
+        "POST",
+        "/lugares",
+        data2,
+        headers,
+    )
+    generalResponse = connection.getresponse()
+    statusCode = generalResponse.status
+    assert HTTPStatus.CREATED == statusCode
+
+def test_GetPlace():
+    connection = httpClient.HTTPConnection("127.0.0.1",9090)
+    headers = {"Content-Type": "application/json"}
+    response = connection.request(
+        "GET",
+        "/lugares/1",
+        "",
+        headers
+    )
+    generalResponse = connection.getresponse()
+    statusCode = generalResponse.status
+    assert statusCode == HTTPStatus.OK
+
+def test_DeletePlace():
+    global token
+    connection = httpClient.HTTPConnection("127.0.0.1",9090)
+    bear = "Bearer " + token
+    headers = {"Content-Type": "application/json","Authorization" : bear}
+    response = connection.request(
+        "DELETE",
+        "/lugares/1",
+        "",
+        headers
+    )
+    generalResponse = connection.getresponse()
+    statusCode = generalResponse.status
+    assert statusCode == HTTPStatus.NO_CONTENT
+
+def test_PostComment():
+    global token
+    connection = httpClient.HTTPConnection("127.0.0.1",9090)
+    data = {
+        "student": "zs19014022",
+        "date": "25/05/2022",
+        "hour" : "12:30",
+        "description" : "Muy agradable la carrera",
+        "forum_id" : 4
+    }
+    bear = "Bearer " + token
+    data2 = json.dumps(data)
+    headers = {"Content-Type": "application/json", "Authorization" : bear}
+    response = connection.request(
+        "POST",
+        "/foros/4/comentarios",
+        data2,
+        headers,
+    )
+    generalResponse = connection.getresponse()
+    statusCode = generalResponse.status
+    assert HTTPStatus.OK == statusCode
